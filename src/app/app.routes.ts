24c8ecfe07b4ce0e792cd428bae797142601ef23
ainhoa <ainhoamiret@gmail.com>
2025-12-16 10:01:51 +0100

    fix conflicts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './components/pages/login/login';
+import { guestGuard } from './guards/guest.guard';
+
+export const routes: Routes = [
+    { path: 'admin/login', component: Login, canActivate: [guestGuard] },
+    { path: '**', redirectTo: 'admin/login' }
+];

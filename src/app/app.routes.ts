1e4c8fca53216aa1d4901f79d79016e824660ee9
ainhoa <ainhoamiret@gmail.com>
2025-12-17 11:39:41 +0100

    fix(Nibiru-Home/sprints#19): App routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { Login } from './components/pages/login/login';
 import { guestGuard } from './guards/guest.guard';
 import { authGuard } from './guards/auth.guard';
 import { Index } from './components/pages/index/index';
-import { Error404 } from './components/pages/error-404/error-404';
 import { ProductsComponent } from './components/pages/products/products';
+import { Error404 } from './components/pages/error-404/error-404';
 
 export const routes: Routes = [
     { path: 'admin/login', component: Login, canActivate: [guestGuard] },
